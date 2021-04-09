10db4b6245bcd0b3c24516a322f70d92af1cf71d
高柳祐貴 <takayanagiyuuki@takayanagiyuukinoMacBook-Pro.local>
2021-04-09 22:52:51 +0900

    JavaScript1-2再提出用　修正完了

--- js/main.js
+++ js/main.js
@@ -1,29 +1,21 @@
 'use strict';
 
-{
-  // do {
-  // }
-  let num = 0;
-  
-  while (num < 5) {
+{  
+  while (true) {
     let result = prompt('日本の首都は？');
-    console.log(result);
   
     if (result === '東京') {
       alert('正解です!');
+      break;
     } 
-    else if(result === null) {
+    else if (result === null) {
       alert('キャンセルされました。');
     }
     else if (result === '') {
       alert('入力されていません。');
     }
-    else if (result != '東京'){
+    else if (result != '東京') {
       alert('不正解です!');
     }
-    if (result === '東京') continue;
-    num++;
-    // console.log(num);
-    // console.log(result);
   }
 }
